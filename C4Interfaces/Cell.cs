fdbdee491008fd7a4f82684ee0283367abf74480
ManuelDeLeon <manueldeleon@gmail.com>
2017-09-06 11:36:07 -0600

    Update Player & Cell

--- C4Interfaces/Cell.cs
+++ C4Interfaces/Cell.cs
@@ -1,9 +1,26 @@
-﻿namespace C4Interfaces
+﻿using System;
+
+namespace C4Interfaces
 {
-	public struct Cell
+	public class Cell: IEquatable<Cell>
 	{
-		public int X { get; set; }
-		public int Y { get; set; }
+		public int X { get; set; } // 1-7
+		public int Y { get; set; } // 1-6
 		public Color Stone { get; set; }
+
+		// Equals & GetHashCode
+		public bool Equals(Cell other)
+		{
+			return this.GetHashCode() == other.GetHashCode();
+		}
+		public override bool Equals(object obj)
+		{
+			var other = obj as Cell;
+			return other != null && Equals(other);
+		}
+		public override int GetHashCode()
+		{
+			return (X * 10) + Y;
+		}
 	}
 }
--- C4Interfaces/Player.cs
+++ C4Interfaces/Player.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace C4Interfaces
 {
 	public abstract class Player
 	{
-		public Color PlayerColor { get; private set; }
+		/// <summary>
+		/// Initialize the player with some information about the game to play
+		/// </summary>
+		/// <param name="playerColor">The color the player is going to play with.</param>
+		/// <param name="totalTimeInSecs">Total game time the player has to play. A player loses if they use more time than this.</param>
+		/// <param name="machineCores">How many logical cores the machine running the game has.</param>
+		public Player(Color playerColor, int totalTimeInSecs, int machineCores) { }
 
-		public Player(Color playerColor)
-		{
-			PlayerColor = playerColor;
-		}
-
-		public abstract int PlayMove(IBoard currentState, DateTime timeRemaining);
+		/// <summary>
+		/// Calculate this player's next move.
+		/// </summary>
+		/// <param name="board">The collection of cells used so far. A cell is empty if it's not on the board collection.</param>
+		/// <returns>The column the player wants to play in.</returns>
+		public abstract int ColumnToPlay(Dictionary<Cell, Color> board);
 	}
 }
