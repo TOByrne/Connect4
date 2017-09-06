9b2391b06a5757e1388b042d74747d43bffcd264
Tony O'Byrne <tg.obyrne@gmail.com>
2017-09-06 08:17:01 -0600

    Changing ICell from an interface to a Struct

---
+++ C4Interfaces/Cell.cs
@@ -0,0 +1,15 @@
+﻿namespace C4Interfaces
+{
+	public struct Cell
+	{
+		int X { get; set; }
+		int Y { get; set; }
+	}
+
+	public enum SquareValue
+	{
+		Red,
+		Yellow,
+		Empty
+	}
+}
--- C4Interfaces/IBoard.cs
+++ C4Interfaces/IBoard.cs
@@ -1,15 +1,4 @@
-﻿/*
-	Dictionary<ICell, Square.SquareValue State> State { get; set; } is more complicated than
-	ICell[,] State { get; set; } since we'd have to track every instance of ICell created.
-
-	Using the dictionary doesn't allow us to reference a square using
-		State[new ICell{ X = x, Y = y }]
-	as that new ICell is a new reference.
-	Assert.AreEqual(Square.SquareValue.Empty, board.State[new Square {X = x, Y = y}]);
-		(Fails)
-
-	ICell[x,y] is just WAY easier and doesn't require any extra tracking.
- */
+﻿using System.Collections.Generic;
 
 namespace C4Interfaces
 {
@@ -17,8 +6,7 @@ namespace C4Interfaces
 	{
 		int Width { get; }		//	Readonly
 		int Height { get; }		//	Readonly
-		//Dictionary<ICell, SquareValue> State { get; set; }
 
-		ICell[,] State { get; set; }
+		Dictionary<Cell, SquareValue> State { get; set; }
 	}
 }
